78a4e19370f0d2e62c38a42568131cad5da18257
YimKeul <leesjun29@gmail.com>
2023-04-04 13:56:17 +0900

    feat : project service

---
+++ src/main/java/com/cnu/real_coding_server/repository/ProjectRepository.java
@@ -0,0 +1,8 @@
+package com.cnu.real_coding_server.repository;
+
+import com.cnu.real_coding_server.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+
+}
