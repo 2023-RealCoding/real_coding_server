3725e6920a9a294480cab9b018bd6a5f48a90041
Minju Song <tisu79@naver.com>
2023-04-19 21:43:11 +0900

    assignment

--- src/main/java/com/cnu/real_coding_server/entity/BaseEntity.java
+++ src/main/java/com/cnu/real_coding_server/entity/BaseEntity.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+    
     @Column
     @CreatedDate
     private LocalDateTime createdAt;
