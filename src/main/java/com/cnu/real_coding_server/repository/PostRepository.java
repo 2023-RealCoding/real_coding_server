11782cdc92d4240d7637f8be3fc72b815927f7a7
TAEHONG-LEE <2t_hong@naver.com>
2023-04-21 13:48:54 +0900

    Assignment

--- src/main/java/com/cnu/real_coding_server/repository/PostRepository.java
+++ src/main/java/com/cnu/real_coding_server/repository/PostRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
 //    Post save(Post post);
-//    List<Post> findAll();
-//    Optional<Post> findById(Integer postId);
+//    List<Post> findAll(); 모든 정보를 볼 수 있도록 하는 함수이다.
+//    Optional<Post> findById(Integer postId); 아이디를 알고 있다면 특정 게시물을 불러와줘
 //    void delete(Post post);
 }
